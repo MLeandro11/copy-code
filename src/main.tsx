4d86ef6dc176767b896a017a668c20d8532961f3
leandro <leandrolm87@gmail.com>
2025-01-18 16:46:24 -0300

    correccion de codigos

--- src/main.tsx
+++ src/main.tsx
@@ -14,8 +14,8 @@ const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
-    // <StrictMode>
-    // </StrictMode>
-    <RouterProvider router={router} />
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
   );
 }
--- src/routes/codes.lazy.tsx
+++ src/routes/codes.lazy.tsx
@@ -3,7 +3,7 @@ import { ArrowLeft } from "lucide-react";
 import { useEffect, useState } from "react";
 import { database } from "@/firebase/config";
 import { ScannedCode } from "@/interfaces/code.interface";
-import { onChildAdded, onValue, ref } from "firebase/database";
+import { onValue, ref } from "firebase/database";
 import CodeList from "@/components/CodeList";
 
 export const Route = createLazyFileRoute("/codes")({
